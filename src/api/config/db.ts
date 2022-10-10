129951ddba634060791f9d5862c181d25362b204
Kuba Góra <gorakuba083@gmail.com>
2022-10-10 15:00:44 +0200

    app corrects 10.10 v1

--- src/api/config/db.ts
+++
@@ -1,9 +0,0 @@
-const { createPool } = require('mysql2');
-const databaseConnection = createPool({
-  host: 'sql11.freesqldatabase.com',
-  user: 'sql11524673',
-  password: 'bCgMd4F5h6',
-  database: 'sql11524673',
-});
-
-module.exports = databaseConnection;
