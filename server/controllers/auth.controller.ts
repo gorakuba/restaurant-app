59fa87cdfed08c464ae8fa6b48ad5b45330dacd8
Kuba Góra <gorakuba083@gmail.com>
2022-11-01 22:42:18 +0100

    api

--- server/controllers/auth.controller.ts
+++ server/controllers/auth.controller.ts
@@ -12,30 +12,20 @@ exports.signin = (req, res) => {
 
 exports.signup = (req, res) => {
   const { username, password, email } = req.body;
-  const saltRounds = 10;
-  let hashedPassword: string = '';
 
+  const saltRounds = 10;
   bcrypt.genSalt(saltRounds, (err, salt) => {
     bcrypt.hash(password, salt, (err, hash) => {
-      hashedPassword = hash;
-      return hashedPassword;
+      const addUserQuery = `INSERT INTO users (username, password, email) VALUES ('${username}', '${hash}', '${email}')`;
+
+      dbConnection.query(addUserQuery, (error, results) => {
+        if (error) {
+          res.send(error.sqlMessage);
+        } else {
+          console.log('user added successfully!');
+          res.redirect('/login-form');
+        }
+      });
     });
   });
-
-  console.log(hashedPassword);
-
-  const addUserQuery = `INSERT INTO users (username, password, email) VALUES ('${username}', '${hashedPassword}', '${email}')`;
-
-  dbConnection.query(addUserQuery, (error, results) => {
-    if (error) {
-      res.send('user with this email or username alreay exist!');
-      res.redirect('/register-form');
-      return console.error(error);
-    } else {
-      console.log('user added successfully!');
-      res.redirect('/login-form');
-    }
-  });
-
-  dbConnection.end();
 };
